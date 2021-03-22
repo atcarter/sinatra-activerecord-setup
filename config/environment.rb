c0e428fa5d7b32081a43584758b65df85e9361c9
Antonio <atcarter005@gmail.com>
2021-03-22 18:04:04 -0500

    commit tree 72deb0c594d9ac745bd0afc4c30dddcef13c2ba6

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,8 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
+configure :development do
+    set :database, 'sqlite3:db/database.db' #change database to the name of whatever objects you're persisting
+end 
+
 require './app'
